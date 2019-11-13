b5af45214181f6faf816e0a6de12b09daf75bae0
ASAP20 <agungtwenty@gmail.com>
2019-11-13 13:34:42 +0700

    Sukses

---
+++ app/src/main/java/asap20/com/lestarimulya/TarikTabunganActivity.java
@@ -0,0 +1,14 @@
+package asap20.com.lestarimulya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TarikTabunganActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tarik_tabungan);
+    }
+}
